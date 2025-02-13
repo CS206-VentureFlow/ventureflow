79dba82d3e93dbf8e5438917910fcc3fa578788b
ppv-1 <virus9320@gmail.com>
2025-02-13 20:53:51 +0800

    Add dashboard (broken)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Dashboard from './features/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
